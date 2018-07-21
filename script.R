e8979bdc87fa530a1b9a65e5e3d5d85c4d14b5b3
gcmurillo <gcmurillo@outlook.com>
2018-07-21 18:05:47 -0500

    tables with and without header

---
+++ script.R
@@ -0,0 +1,8 @@
+t1 <- read.table("C:\\Users\\geancarlo\\Documents\\GitHub\\R_read_tutorial\\tabla_cabecera1.txt", header=T,sep="\t");
+t1
+
+t2 <- read.table("C:\\Users\\geancarlo\\Documents\\GitHub\\R_read_tutorial\\tabla_cabecera2.txt", header=T,sep=" ");
+t2
+
+t3 <- read.table("C:\\Users\\geancarlo\\Documents\\GitHub\\R_read_tutorial\\tabla_sin_cabecera.txt", header=F);
+t3
